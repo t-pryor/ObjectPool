9ad67088e9623d79e68f813ec5a7974d05f6700a
Tim Pryor <Tim@Chantals-MacBook-Pro.local>
2015-10-06 10:11:59 -0400

    Added Pool.swift file. Pool is a generic class that manages a collection of objects. Because it is generic, it can be reused to manage different types. Pool includes concurrecy techniques to protect its data array.

--- ObjectPool/Pool.swift
+++ ObjectPool/Pool.swift
@@ -17,27 +17,43 @@ class Pool<T> {
     // data array is used like a simple queue collection containing the 
     // objects that are available for use
     private var data = [T]()
+    // arrayQ is a serial queue so that only one block is executed at a time
+    // only one thread can modify the data array
+    private let arrayQ = dispatch_queue_create("arrayQ", DISPATCH_QUEUE_SERIAL)
     
-    
+    // the counter is decremented each time the dispatch_semaphore_wait func is called
+    private let semaphore: dispatch_semaphore_t
     
     init(items:[T]) {
         data.reserveCapacity(data.count)
         for item in items {
             data.append(item)
         }
+        
+        semaphore = dispatch_semaphore_create(items.count)
+        
     }
 
     // return the object at the head of the harray
     func getFromPool() -> T? {
         var result:T?
-        if (data.count > 0) {
-            result = self.data.removeAtIndex(0)
+        // when counter reaches zero, calls to the dispatch_semaphore_wait will block
+        if (dispatch_semaphore_wait(semaphore, DISPATCH_TIME_FOREVER) == 0) {
+            dispatch_sync(arrayQ) {
+                result = self.data.removeAtIndex(0)
+            }
         }
+        
+        
         return result
     }
 
     // return previously used object to the end of the array
     func returnToPool(item:T) {
-        self.data.append(item)
+        dispatch_async(arrayQ) {
+            self.data.append(item)
+            // counter is incremented
+            dispatch_semaphore_signal(self.semaphore)
+        }
     }
 }
